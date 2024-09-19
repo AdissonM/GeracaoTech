5be53d34cff933584e2c60f4969a48b5d9e78191
unknown <ADISSON6174@DIGITALCOLLEGE.LOCAL>
2024-09-19 17:53:18 -0300

    Atualização

---
+++ AULA 1/Teste/Udemy Aula 3 .js
@@ -0,0 +1,6 @@
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 457.832;
+console.log(typeof salario);   
---
+++ AULA 1/Teste/teste da Udemy.js
@@ -0,0 +1,35 @@
+//* AULA 21 & 22 - DESAFIOS & SENTENÇAS *// 
+// Bloco 1 
+1
+3
+5
+7
+9 
+//Bloco 2
+2
+4
+6
+8
+10
+*//
+
+//* AULA 23 - TRABALHANDO DADOS 01 *// 
+
+var nome = "Caneta";
+var quantidade =10;
+var preco = 6.4
+let imposto = 1.5;
+let precoFinal = preco + imposto; 
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
+
+nome = "CanetaBIC";
+console.log(nome)
+
+//* AULA 24 - TRABALHANDO DADOS 02 *// 
+
+
